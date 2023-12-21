5f8a31d60bfdeb89c05cb075cf44fd2aa5183ddc
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-12-21 10:06:22 +0530

    checking

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -11,7 +11,7 @@ final class RMSettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .red
         title = "Settings"
     }
     
